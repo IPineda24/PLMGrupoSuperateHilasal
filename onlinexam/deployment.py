965b7189f41c2fb180305d808aa0667eea541788
irvin pineda <62679993+IPineda24@users.noreply.github.com>
2023-11-07 15:50:04 -0600

    Update deployment.py

--- onlinexam/deployment.py
+++ onlinexam/deployment.py
@@ -28,10 +28,11 @@ conn_str = os.environ['AZURE_SQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlserver',
         'NAME': conn_str_params['dbname'],
         'HOST': conn_str_params['host'],
         'USER': conn_str_params['user'],
         'PASSWORD': conn_str_params['password'],
     }
 }
+
