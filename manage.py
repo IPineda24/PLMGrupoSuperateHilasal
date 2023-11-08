c4846037cb821d4adb97844458add02b7559ac44
irvin pineda <62679993+IPineda24@users.noreply.github.com>
2023-11-07 20:54:53 -0600

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,9 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinexam.settings')
+   
+     settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'onlinexam.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
