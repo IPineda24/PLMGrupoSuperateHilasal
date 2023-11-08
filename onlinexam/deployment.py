55848ef3462e8b5e65377b1a8fcb1d0f04afaf18
irvin pineda <62679993+IPineda24@users.noreply.github.com>
2023-11-07 22:03:38 -0600

    Update deployment.py

--- onlinexam/deployment.py
+++ onlinexam/deployment.py
@@ -35,10 +35,7 @@ conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.s
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
